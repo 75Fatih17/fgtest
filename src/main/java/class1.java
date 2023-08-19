3a25e39d8ef66274192623cca298f2eeb57c56c9
Fatih Gündoğdu <yeni1yaz@gmail.com>
2023-08-19 23:13:00 +0300

    v1

--- src/main/java/class1.java
+++ src/main/java/class1.java
@@ -1,14 +1,10 @@
-public class Class01 {
+public class class1 {
     public static void main(String[] args) {
-
         System.out.println("Versiyon1");
 
         System.out.println("Versiyon2");
 
         System.out.println("Versiyon3");
-
-
-
 
     }
 }
